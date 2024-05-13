936c9d47b6b034a0f83eef468d28ded15b3ef967
EdrhianValerioITICB <2023_edrhian.valerio@iticbcn.cat>
2024-05-13 20:53:25 +0200

    Added unfinished masiv csv

--- app.py
+++ app.py
@@ -1,29 +1,26 @@
 from typing import Union
-from fastapi import FastAPI
+from fastapi import FastAPI, File, UploadFile
 import botiga_db
 from pydantic import BaseModel
 
 app = FastAPI()
 
-# class category():
-#     name: str
-#     create_at: str
-#     update_at: str
+class category(BaseModel):
+    name: str
+    create_at: str
+    update_at: str
 
-# class subcategory():
-#     name: str
-#     category_id: str
-#     create_at: str
-#     update_at: str
-#     category_id: category
+class subcategory(BaseModel):
+    subcategory_id: int
+    category_id: int
 
-# class product(BaseModel):
-#     name: str
-#     description: str
-#     company: str
-#     price: float
-#     units: int
-#     subcategory_id: subcategory
+class product(BaseModel):
+    name: str
+    description: str
+    company: str
+    price: float
+    units: int
+    subcategory: subcategory
 
 @app.get("/products")
 def read_products():
@@ -34,8 +31,14 @@ def read_one_product(id: int):
     return botiga_db.read_one(id)
 
 @app.post("/product")
-def create_one_product(data):
-    return botiga_db.create(data)
+def create_one_product(data: product):
+    name = data.name
+    description = data.description
+    company = data.company
+    price = data.price
+    units = data.units
+    subcategory_id = data.subcategory_id
+    return botiga_db.afegir_producte(name, description, company, price, units, subcategory_id)
 
 @app.put("/product/{id}")
 def update_product(id: int):
@@ -49,5 +52,6 @@ def delte_product(id: int):
 def getAll_products():
     pass
 
-
-
+@app.post("/massivo")
+async def carrega_massiva(file: UploadFile):
+    return botiga_db.carregar_csv(file)
